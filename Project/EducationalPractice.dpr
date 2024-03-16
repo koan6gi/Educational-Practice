d3e4753f17661dd9bd1a1cec1c40379c1a8243b8
koan6gi <rgolovkl6@gmail.com>
2024-03-16 15:13:42 +0300

    Create a Type

--- Project/EducationalPractice.dpr
+++ Project/EducationalPractice.dpr
@@ -1,12 +1,42 @@
 program EducationalPractice;
 
-{$APPTYPE CONSOLE}
+Type
+  TypeOfList = (Artist, Album, Song);
 
-{$R *.res}
+  TAdr = ^TList;
 
-uses
-  System.SysUtils;
+  TArtist = record
+    ID: Integer;
+    Name: String[20];
+    Country: String[20];
+    Direction: String[20];
+  end;
+
+  TAlbum = record
+    ID: Integer;
+    ID_Artist: Integer;
+    Name: String[20];
+    Year: Integer;
+  end;
+
+  TSong = record
+    Name: String[20];
+    ID_Album: Integer;
+    Length: Integer;
+  end;
+
+  TList = record
+    next: TAdr;
+    case TypeOfList of
+      Artist:
+        (Artist: TArtist);
+      Album:
+        (Album: TAlbum);
+      Song:
+        (Song: TSong);
+  end;
+
 
 begin
-  
+
 end.
