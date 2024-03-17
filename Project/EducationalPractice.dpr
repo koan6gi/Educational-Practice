ca4e8584994e3efc95766be3277d54525a381f07
koan6gi <rgolovkl6@gmail.com>
2024-03-17 14:11:14 +0300

    Work with files in Unit

--- Project/EducationalPractice.dpr
+++ Project/EducationalPractice.dpr
@@ -1,107 +1,15 @@
 program EducationalPractice;
 
 uses
-  System.SysUtils;
-
-Type
-  TypeOfList = (Artist, Album, Song);
-
-  TAdrOfList = ^TList;
-
-  TArtist = record
-    ID: Integer;
-    Name: String[20];
-    Country: String[20];
-    Direction: String[20];
-  end;
-
-  TArtistFile = File of TArtist;
-
-  TAlbum = record
-    ID: Integer;
-    ID_Artist: Integer;
-    Name: String[20];
-    Year: Integer;
-  end;
-
-  TAlbumFile = File of TAlbum;
-
-  TSong = record
-    Name: String[20];
-    ID_Album: Integer;
-    Length: Integer;
-  end;
-
-  TSongFile = File of TSong;
-
-  TList = record
-    next: TAdrOfList;
-    Max_Id: Integer;
-    case ListType: TypeOfList of
-      Artist:
-        (Artist: TArtist);
-      Album:
-        (Album: TAlbum);
-      Song:
-        (Song: TSong);
-  end;
-
-Procedure ReadArtistListFromFile(ArtistList: TAdrOfList;
-  var ArtistFile: TArtistFile);
-var
-  MaxID: ^Integer;
-begin
-  Assign(ArtistFile, GetCurrentDir() + '\ArtistFile');
-  Reset(ArtistFile);
-  MaxID := @ArtistList^.Max_Id;
-  repeat
-    New(ArtistList^.next);
-    ArtistList := ArtistList^.next;
-    Inc(MaxID^);
-    Read(ArtistFile, ArtistList^.Artist);
-  until Eof(ArtistFile);
-  Close(ArtistFile);
-end;
-
-Procedure ReadAlbumListFromFile(AlbumList: TAdrOfList;
-  var AlbumFile: TAlbumFile);
-var
-  MaxID: ^Integer;
-begin
-  Assign(AlbumFile, GetCurrentDir() + '\AlbumFile');
-  Reset(AlbumFile);
-  MaxID := @AlbumList^.Max_Id;
-  repeat
-    New(AlbumList^.next);
-    AlbumList := AlbumList^.next;
-    Inc(MaxID^);
-    Read(AlbumFile, AlbumList^.Album);
-  until Eof(AlbumFile);
-  Close(AlbumFile);
-end;
-
-Procedure ReadSongListFromFile(SongList: TAdrOfList; var SongFile: TSongFile);
-var
-  MaxID: ^Integer;
-begin
-  Assign(SongFile, GetCurrentDir() + '\SongFile');
-  Reset(SongFile);
-  MaxID := @SongList^.Max_Id;
-  repeat
-    New(SongList^.next);
-    SongList := SongList^.next;
-    Inc(MaxID^);
-    Read(SongFile, SongList^.Song);
-  until Eof(SongFile);
-  Close(SongFile);
-end;
+  WorkWithFiles in 'WorkWithFiles.pas',
+  AllTypesInProject in 'AllTypesInProject.pas';
 
 var
   ArtistList, AlbumList, SongList: TAdrOfList;
   ArtistFile: TArtistFile;
   AlbumFile: TAlbumFile;
   SongFile: TSongFile;
-  Menu: Integer;
+  Menu: Integer = 0;
 
 begin
   New(ArtistList);
@@ -119,9 +27,8 @@ begin
     case Menu of
       1:
         begin
-          ReadArtistListFromFile(ArtistList, ArtistFile);
-          ReadAlbumListFromFile(AlbumList, AlbumFile);
-          ReadSongListFromFile(SongList, SongFile);
+          ReadAllListsFromFiles(ArtistList, AlbumList, SongList, ArtistFile,
+            AlbumFile, SongFile);
         end;
 
       2:
@@ -159,12 +66,13 @@ begin
 
         end;
 
-      10:
+      9:
         begin
-
+          ReWriteAllListsInFiles(ArtistList, AlbumList, SongList, ArtistFile,
+            AlbumFile, SongFile);
         end;
     end;
-  Until (Menu = 9);
+  Until (Menu = 0) or (Menu = 9);;
 
   readln;
 
